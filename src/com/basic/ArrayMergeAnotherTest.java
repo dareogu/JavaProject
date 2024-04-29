cd9b58a036acdb7cb14dc663fff80151b2ffdfae
dareogu <njhmwx@163.com>
2024-04-29 10:24:18 +0800

    add new class_test

--- src/com/basic/ArrayMergeAnotherTest.java
+++ src/com/basic/ArrayMergeAnotherTest.java
@@ -8,27 +8,21 @@ import java.util.Arrays;
  * @author Dareo_Gu
  * 2017-11-18
  */
-public class ArrayMergeTest {
+public class ArrayMergeAnotherTest {
   public static void main(String[] args) {
     int[] a = {9, 11, 16, 28, 67, 75};
     int[] b = {8, 26, 30, 48, 56, 66, 93, 97, 99};
     int[] c = new int[a.length + b.length];
-    int i = 0, j = 0, k = 0;
-    while (i < a.length && j < b.length) {
-      if (a[i] < b[j]) {
-        c[k] = a[i];
-        i++;
+    for (int cIndex = 0, aIndex = 0, bIndex = 0; cIndex < c.length; cIndex++) {
+      if (aIndex >= a.length) {
+        c[cIndex] = b[bIndex++];
+      } else if (bIndex >= b.length) {
+        c[cIndex] = a[aIndex++];
+      } else if (a[aIndex] < b[bIndex]) {
+        c[cIndex] = a[aIndex++];
       } else {
-        c[k] = b[j];
-        j++;
+        c[cIndex] = b[bIndex++];
       }
-      k++;
-    }
-    if (i < a.length) {
-      System.arraycopy(a, i, c, k, a.length - i);
-    }
-    if (j < b.length) {
-      System.arraycopy(b, j, c, k, b.length - j);
     }
     System.out.println(Arrays.toString(c));
   }
